146af31833bf1936297fc5d44237a8dd972bd434
Nicki Klein <nicoleklein@me.com>
2024-02-06 13:50:47 -0800

    update the about

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ class About extends React.Component {
         <div className="choose animate-box">
             <div className="colorlib-heading">
                 <h2>44 Years of Experience in Worker's Comp</h2>
-                <p>Getting injured in the workplace can be physically, financially, and mentally draining for everyone involved. If you have been injured on the job, you must navigate through the insurance process for injuries. We will help you navigate the insurance process to receiving the highest compensation for your injury. Without a seasoned Worker's Compensation attorney such as Jeffery Klein, you can fall victim to several pitfalls along the way that would permanently affect your case and compensation. We are here to help you every step of the way.</p>
+                <p>Getting injured in the workplace can be physically, financially, and mentally draining for everyone involved. We will help you every step of the way including navigating through the insurance process to receive the highest compensation for your injury. Jeffery Klein is a caring honest attorney with 44 years of experience who has represented both sides of litigation and has successfully won awards of 1M+ for his clients.</p>
             </div>
        </div>
     </div>)
